89dc745406e8400346a0e5ff04b174fb94c97534
Jose Farrugia <jose@josefarrugia.com>
2024-06-03 16:09:53 +0100

    feat: add calculator page view

---
+++ pages/calculator.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="container mx-auto translate-y-12 lg:translate-y-24">
+    <CalculatorBase/>
+  </section>
+</template>
+
+<script setup lang="ts">
+import CalculatorBase from "~/components/Calculator/CalculatorBase.vue";
+</script>
