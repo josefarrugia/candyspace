f0c5db90ffa941d09f1f2638e4aaec701a9099dc
Jose Farrugia <jose@josefarrugia.com>
2024-06-04 18:13:27 +0100

    feat: add Image component

---
+++ components/Image.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+  src?: string,
+  alt?: string,
+}>()
+
+</script>
+
+<template>
+  <!-- @TODO: add srcset/sizes attribute to make image more responsive and performant -->
+  <div>
+    <img
+        :src="props.src"
+        :alt="props.alt"
+        class="bg-gray-300"
+        :class="$attrs.class"
+    />
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+div {
+  aspect-ratio: 8/5;
+  @apply bg-gray-50;
+}
+</style>
+
