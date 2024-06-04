6a2a33b08bfce91765ad7413b4d29bfa96bd7d62
Jose Farrugia <jose@josefarrugia.com>
2024-06-04 18:16:47 +0100

    feat: add makeServer and typeface to app.vue

--- app.vue
+++ app.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+import {makeServer} from "~/server";
+
+if (process.env.NODE_ENV === "development") {
+  makeServer()
+}
+
+
+</script>
+
 <template>
   <div>
     <NuxtPage/>
@@ -6,4 +16,5 @@
 
 <style lang="postcss">
 @import url('https://fonts.googleapis.com/css2?family=PT+Mono&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap');
 </style>
