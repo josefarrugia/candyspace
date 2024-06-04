06668882e0009ec5968a68db1ecb4e080ff55c15
Jose Farrugia <jose@josefarrugia.com>
2024-06-04 18:13:02 +0100

    feat: add Block component

---
+++ components/Block.vue
@@ -0,0 +1,6 @@
+<template>
+  <div
+      class="p-6 rounded-xl bg-white border-b-2 border-b-blue-200 hover:shadow-xl hover:shadow-blue-100 transition-all duration-500">
+    <slot/>
+  </div>
+</template>
