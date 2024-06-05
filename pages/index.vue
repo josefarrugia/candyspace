6c0f299df64fafae535aa8369a1547887335f764
Jose Farrugia <jose@josefarrugia.com>
2024-06-05 15:02:57 +0100

    feat: add index page

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="h-screen">
+    <div class="flex justify-center items-center h-full max-w-screen-sm mx-auto font-noto-sans">
+      <div>
+        <h1 class="font-bold">Welcome 👋🏽</h1>
+        <h2 class="text-sm">By Jose Farrugia</h2>
+        <div class="mt-8">
+          <h3 class="underline underline-offset-2">
+            <NuxtLink to="/calculator">Calculator</NuxtLink>
+          </h3>
+          <h3 class="underline underline-offset-2">
+            <NuxtLink to="/cars">Car Listing Page</NuxtLink>
+          </h3>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
